b196115f9e5feb6f3d4d902c346c0ef78619733c
Benoit Calin <benoit.calin@gmail.com>
2019-02-18 18:10:59 +0100

    Add seed 3xUsers 3xElders 3xRents

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,37 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Rent.destroy_all
+Elder.destroy_all
+User.destroy_all
+
+puts 'Start seed'
+
+User.create!(first_name: 'Benoit', last_name: 'Calin', email: 'benoit.cal@grandpa-rent.com', password:'123soleil')
+User.create!(first_name: 'Xeniia', last_name: 'Gavr', email: 'xeniia.g@grandpa-rent.com', password:'123soleil')
+User.create!(first_name: 'Kenza', last_name: 'Ahed', email: 'kenza.a@grandpa-rent.com', password:'123soleil')
+
+e = Elder.new(full_name: 'Bébère', address: 'EPHAD n°1, Bordeaux')
+e.user = User.first
+e.save!
+e = Elder.new(full_name: 'Nanard', address: 'EPHAD n°3, Bordeaux')
+e.user = User.last
+e.save!
+e = Elder.new(full_name: 'Georges', address: 'EPHAD n°2, Bordeaux')
+e.user = User.last
+e.save!
+
+d1 = Date.new(2019, 4, 1)
+d2 = Date.new(2019, 4, 5)
+
+r = Rent.new(start_date: d1, end_date: d2)
+r.user = User.find(2)
+r.elder = Elder.first
+r.save!
+r = Rent.new(start_date: d1, end_date: d2)
+r.user = User.first
+r.elder = Elder.find(2)
+r.save!
+r = Rent.new(start_date: d1, end_date: d2)
+r.user = User.last
+r.elder = Elder.first
+r.save!
+
+puts 'The end'
