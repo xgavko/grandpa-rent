cae4d97a9c6e6b84c21e8748c8a459b25952716e
Benoit Calin <benoit.calin@gmail.com>
2019-02-21 12:02:50 +0100

    Add migration lat & long on elders & Add geocoding to Elder Model

--- app/models/elder.rb
+++ app/models/elder.rb
@@ -9,4 +9,7 @@ class Elder < ApplicationRecord
   validates :address, presence: true
 
   mount_uploader :photo, PhotoUploader
+
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_20_133210) do
+ActiveRecord::Schema.define(version: 2019_02_21_110047) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,8 @@ ActiveRecord::Schema.define(version: 2019_02_20_133210) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "photo"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_elders_on_user_id"
   end
 
