4ff8fb628c8cb23b20e0d934a918fea20152734e
nicolas beaudouin <nicolas.pn.beaudouin@gmail.com>
2019-02-21 16:53:35 +0100

    status text changes color, buttons are disabled

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,20 +5,10 @@ import 'flatpickr/dist/flatpickr.min.css';
 import 'mapbox-gl/dist/mapbox-gl.css';
 
 import { initMapbox } from '../plugins/init_mapbox';
-<<<<<<< HEAD
 import { initFlatpickr } from '../plugins/init_flatpickr';
-import { changeStatus } from 'rents_status';
-=======
 import { initSelect2 } from '../plugins/init_select2.js';
-import {} from '../plugins/init_flatpickr.js';
->>>>>>> 4c50e1dee06ced61c0924c298d4303fd82544c84
 
 initSelect2();
-<<<<<<< HEAD
 initFlatpickr();
-
-
-
-=======
 initMapbox();
->>>>>>> 4c50e1dee06ced61c0924c298d4303fd82544c84
+
