2624be7a2b9feccc947e6e59a7b4bd114fe2049d
Benoit Calin <benoit.calin@gmail.com>
2019-02-21 16:14:38 +0100

    Correct JS

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,14 +1,12 @@
 import "bootstrap";
 
 import 'select2/dist/css/select2.css';
+import 'flatpickr/dist/flatpickr.min.css';
 import 'mapbox-gl/dist/mapbox-gl.css';
-import "flatpickr/dist/flatpickr.min.css";
 
-import { initSelect2 } from '../plugins/init_select2.js';
 import { initMapbox } from '../plugins/init_mapbox';
-import { changeStatus } from "./rents_status.js";
-import { initFlatpickr } from '../plugins/init_flatpickr';
+import { initSelect2 } from '../plugins/init_select2.js';
+import {} from '../plugins/init_flatpickr.js';
 
-initMapbox();
 initSelect2();
-initFlatpickr();
+initMapbox();
